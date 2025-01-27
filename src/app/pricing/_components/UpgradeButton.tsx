36427cbcce214d689cb27b6900717f26e1407bec
Abhias2405 <iamabhisheksingh56@gmail.com>
2025-01-27 23:24:00 +0530

    env update

--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -2,11 +2,11 @@ import { Zap } from "lucide-react";
 import Link from "next/link";
 
 export default function UpgradeButton() {
-  const CHEKOUT_URL = "https://abhisheksingh.lemonsqueezy.com/buy/598ad862-36aa-4b06-91c6-eb496cc6ad72";
+  const CHEKOUT_URL = process.env.NEXT_PUBLIC_CHECKOUT_URL;
 
   return (
     <Link
-      href={CHEKOUT_URL}
+      href={CHEKOUT_URL || "#"} 
       className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
         bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
         hover:from-blue-600 hover:to-blue-700 transition-all"
